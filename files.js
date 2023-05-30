60684cc5dc41983a8127c02deb44f327101cba21
haroldmud <haroldmudosa@gmail.com>
2023-05-30 15:52:23 +0200

    Uncommented some

--- files.js
+++ files.js
@@ -1,28 +1,28 @@
 const fileModifier =  require('fs');
 
 //Reading files
-// fileModifier.readFile('./doc/blog1.txt', (err, data)=>{
-//   if(err){
-//     console.log(err)  //This error will be thrown when the file wont be accessed
-//   }
-//   console.log(data)   //<Buffer 20 58 4f 20 74 69 6c 20 77 65 20 6f 76 65 72 64 6f 73 65>
-//   console.log(data.toString())  // this buffer will be converted to the actual value
-// })
+fileModifier.readFile('./doc/blog1.txt', (err, data)=>{
+  if(err){
+    console.log(err)  //This error will be thrown when the file wont be accessed
+  }
+  console.log(data)   //<Buffer 20 58 4f 20 74 69 6c 20 77 65 20 6f 76 65 72 64 6f 73 65>
+  console.log(data.toString())  // this buffer will be converted to the actual value
+})
 
 
 //Writing files
 
-// fileModifier.writeFile('./doc/blog1.txt', 'Always say less than necessary', ()=>{
-//   return null;
-// })
+fileModifier.writeFile('./doc/blog1.txt', 'Always say less than necessary', ()=>{
+  return null;
+})
 
-// fileModifier.writeFile('./doc/blogX.txt', 
-//     'During hard times, don\'t you ever forget keeping your composure', ()=>{
-//   return null;
-// })
+fileModifier.writeFile('./doc/blogX.txt', 
+    'During hard times, don\'t you ever forget keeping your composure', ()=>{
+  return null;
+})
 
 
-// creating directoriesgit in
+// creating directories
 fileModifier.mkdir('./assets', (err)=>{
   if(err){
     console.log('an error occured')
