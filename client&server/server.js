eb650eef2cb215231a57a56b3d79106cbc9bc5ee
haroldmud <haroldmudosa@email.com>
2023-08-05 11:24:58 +0200

    feat: add the html file that needs to be sent as response

--- client&server/server.js
+++ client&server/server.js
@@ -1,9 +1,21 @@
-const http =require('http');
+const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer((req, res)=>{
-   console.log('the request is already made')
+const server =  http.createServer((req, res )=>{
+  console.log(req);
+  
+  //step1: define the type of the content we'll send as
+  //       a response. this can be a text, an HTML, etc 
+  res.setHeader('content-type', 'text/plain')
+
+  //step2: Write the content according to its type
+  res.write('Hello, world')
+
+  //step3: we end the response which then sends it to the
+  //       browser
+  res.end()
 });
 
-server.listen(3000, 'localhost',()=>{
-  console.log('Waiting for any upcoming request')
+server.listen(8080, 'localhost', ()=>{
+  console.log('waiting for the first listened server request')
 })
