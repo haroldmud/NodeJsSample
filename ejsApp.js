721ee1aa7fa0ebe452c71533e0458c8e174e5470
haroldmud <haroldmudosa@email.com>
2023-08-08 16:09:00 +0200

    feat: add partials

--- ejsApp.js
+++ ejsApp.js
@@ -5,12 +5,30 @@ const app = express()
 // registering the view engine
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res)=>{
-  res.render('index')
-})
 app.listen('8080', ()=> {
   console.log('the request has been sent')
 });
+
+app.get('/', (req, res)=>{
+  const blogs = [
+  {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'}, 
+  {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},  
+  ];
+  res.render('index', {title: 'Home page', blogs })
+});
+
+app.get('/about', (req, res)=>{
+  res.render('about', {title: 'About page'})
+});
+
+app.get('/blogs/create', (req, res)=>{
+  res.render('create', {title: 'Create Blog page'})
+});
+
+app.use((res,req)=>{
+  res.render('404', {title: 'Error  page'})
+})
 
 http.createServer(()=>{
   console.log('the response has been provided')
--- express/app.js
+++ express/app.js
@@ -1,32 +1,31 @@
-const express = require('express');// returns a function
-const path = require('path')
+const express = require("express"); // returns a function
+const path = require("path");
 
 // express app
-const app = express( )
+const app = express();
 
 // listen for requests
-app.listen(3000, ()=>{
-  console.log('the request has been made')
+app.listen(3000, () => {
+  console.log("the request has been made");
 });
 
-app.get('/', (req, res)=>{
-  const homePath = path.join(__dirname, '../view/index.html')
+app.get("/", (req, res) => {
+  const homePath = path.join(__dirname, "../express-view/index.html");
   res.sendFile(homePath);
-})
+});
 
-app.get('/about', (req, res)=>{
-  const aboutPath = path.join(__dirname, '../view/about.html')// the "__dirname" refers to the whole system directory
+app.get("/about", (req, res) => {
+  const aboutPath = path.join(__dirname, "../express-view/about.html"); // the "__dirname" refers to the whole system directory
   res.sendFile(aboutPath);
-})
+});
 
 // redirect
-app.get ('/about-us', (req, res) => {
-  res.redirect('/about')
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
 });
 
-
 // error page handler
-app.use((req, res)=> {
-  const errorPath = path.join( __dirname, '../view/404.html')
-  res.status(404).sendFile(errorPath)
-})
+app.use((req, res) => {
+  const errorPath = path.join(__dirname, "../express-view/404.html");
+  res.status(404).sendFile(errorPath);
+});
